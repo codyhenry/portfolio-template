a64ddfa8eb14c8d863739053c854fc3ad1e65a63
codyhenry <codyhenry527@gmail.com>
2023-02-10 16:06:32 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { HashRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World </h1>
+    </div>
+  );
+}
+
+function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
+
+export default WrappedApp;
