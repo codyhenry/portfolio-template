5a08b65f3de43f148cc5497dccb737fd9b99b133
codyhenry <codyhenry527@gmail.com>
2023-03-11 12:58:09 -0500

    add framer-motion

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,14 +7,15 @@ function Home() {
   return (
     <div>
       <Typography variant="h1" color="primary">
-        Hello World!
+        Put basic info here
+      </Typography>
+      <Typography variant="h3" color="text.primary">
+        School, hobbies, current projects, &quot;tell me about yourself&quot;
+        Links to job sites Handshake, LinkedIn, Indeed, Github
       </Typography>
       <Card />
       <Spacer side="top" size="xxl" />
       <Card />
-      <Typography variant="h1" color="primary">
-        Hello World!
-      </Typography>
     </div>
   );
 }
