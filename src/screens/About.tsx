b5d299ac7d79ab5f936bce2d6a5d48d21b995632
codyhenry <codyhenry527@gmail.com>
2023-03-22 17:49:52 -0400

    Added links to about page sections

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,19 +1,33 @@
-import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import tools from "../components/skills/tool-group.component";
 import languages from "../components/skills/language-group.component";
 import GroupCard from "../components/ui/group.card.component";
 
+const SectionLink = styled("a")(({ theme }) => ({
+  color: theme.palette.text.primary,
+  textDecoration: "none",
+}));
+
+// IMPORTANT: add progress bar for scroll distance make sectionlinks sticky so they scroll too
+// IMPORTANT: add animated arrows pointing down
+// IMPORTANT: center the typography
 export default function About() {
-  // Jump to sections
   return (
     <>
-      <a href="#tools-section">
-        <Button sx={{ color: "#fff" }}>Tools</Button>
-      </a>
+      <Typography variant="h1">My Skills</Typography>
+      <Typography variant="h4">
+        Snippet explaining stuff about skills
+      </Typography>
+      {/* Jump to sections */}
+      <SectionLink href="#languages-section">Languages</SectionLink>
+      <SectionLink href="#tools-section">Tools</SectionLink>
       {/* Iterate through languages */}
-      {languages.map((subGrouping) => (
-        <GroupCard items={subGrouping} key={subGrouping[0].name} />
-      ))}
+      <div id="languages-section">
+        {languages.map((subGrouping) => (
+          <GroupCard items={subGrouping} key={subGrouping[0].name} />
+        ))}
+      </div>
       <div id="tools-section">
         {/* Iterate through tools */}
         {tools.map((subGrouping) => (
